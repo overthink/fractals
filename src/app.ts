2b80ec6c9632dd38862a53e77d2759b606cf6fa7
Mark Feeney <mark.feeney@gmail.com>
2017-02-25 19:24:45 -0500

    Add overlay canvas
    Preparation for drawing select box for zooming.

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,5 @@
 type Margin = {top: number; right: number; bottom: number; left: number};
 
-function disableScrollbars(canvas: HTMLCanvasElement): void {
-    // canvas is display:inline(-block?) by default, apparently
-    // display:block prevents scrollbars; don't fully get it.
-    // http://stackoverflow.com/a/8486324/69689
-    canvas.style.display = "block";
-}
-
-/** Return a new canvas with given width, height, and margin. */
-function createCanvas(width: number, height: number): HTMLCanvasElement {
-    const canvas = document.createElement("canvas");
-    // Don't use CSS to set width/height, see: http://stackoverflow.com/a/12862952/69689
-    canvas.setAttribute("width", width.toString());
-    canvas.setAttribute("height", height.toString());
-    disableScrollbars(canvas);
-    return canvas;
-}
-
 /** Return the viewport size in pixels: width, height. */
 function viewportSize(): [number, number] {
     const de = document.documentElement;
@@ -26,22 +9,38 @@ function viewportSize(): [number, number] {
 }
 
 /**
- * Return the element from the page that will hold the canvas. The returned
- * element is styled to take up the whole viewport, with given margins and
- * border thickness.
+ * Return the element from the page that will hold the canvas.
  */
-function getCanvasContainer(margin: Margin, borderStyle: string): Element {
+function getCanvasContainer(): HTMLElement {
     const id = "#canvas-container";
     const container = document.querySelector(id);
     if (!container || !(container instanceof HTMLElement)) {
         throw new Error(`no HTMLElement found with selector ${id}`);
     }
+    return container;
+}
+
+/**
+ * Style container to take up the whole viewport, with given margins and
+ * border thickness.
+ */
+function styleCanvasContainer(
+        container: HTMLElement,
+        width: number,
+        height: number,
+        margin: Margin,
+        borderStyle: string): Element {
     const s = container.style;
+    s.width = width.toString() + "px";
+    s.height = height.toString() + "px";
     s.marginTop = margin.top + "px";
     s.marginRight = margin.right + "px";
     s.marginBottom = margin.bottom + "px";
     s.marginLeft = margin.left + "px";
     s.border = borderStyle;
+    // Need to explicitly position this container so the child canvas elements
+    // can be positioned absolute.
+    s.position = "relative";
     return container;
 }
 
@@ -90,9 +89,9 @@ function drawMandlebrot(
     const data: Uint8ClampedArray = imageData.data;
     console.log(`Need to draw ${data.length / 4} pixels`);
 
-    // let z[0] = 0
-    // then c is in the set if |z[n]| remains bounded for arbitrarily large n in
-    // z[n+1] = z[n]^2 + c
+    // let z[0] = 0 and c be an arbitrary complex number
+    // then c is in the Mandelbrot set if |z[n]| remains bounded for
+    // arbitrarily large n in z[n+1] = z[n]^2 + c
 
     // differently, P[c] is a complex polynomial (fn):
     // P[c](z) = z^2 + c (think of c as x,y coords on a canvas)
@@ -133,19 +132,79 @@ function getMouseCoords(canvas: HTMLCanvasElement, e: MouseEvent): [number, numb
     return [e.x - rect.left, e.y - rect.top];
 }
 
+/**
+ * A canvas element with a second transparent canvas element (the "overlay")
+ * positioned right on top of it.
+ */
+class OverlayCanvas {
+
+    readonly width: number;
+    readonly height: number;
+    readonly background: HTMLCanvasElement;
+    readonly overlay: HTMLCanvasElement;
+    readonly backgroundCtx: CanvasRenderingContext2D;
+    readonly overlayCtx: CanvasRenderingContext2D;
+
+    private static get2DContext(canvas: HTMLCanvasElement): CanvasRenderingContext2D {
+        const ctx = canvas.getContext("2d");
+        if (!ctx) {
+            throw new Error(`Could not get 2d context for ${canvas}`);
+        }
+        return ctx;
+    }
+
+    private static styleCanvas(canvas: HTMLCanvasElement, zIndex: number): HTMLCanvasElement {
+        const s = canvas.style;
+        s.position = "absolute";
+        s.left = "0px";
+        s.top = "0px";
+        s.zIndex = zIndex.toString();
+        return canvas;
+    }
+
+    private static disableScrollbars(canvas: HTMLCanvasElement): HTMLCanvasElement {
+        // canvas is display:inline(-block?) by default, apparently
+        // display:block prevents scrollbars; don't fully get it.
+        // http://stackoverflow.com/a/8486324/69689
+        canvas.style.display = "block";
+        return canvas;
+    }
+
+    private static createCanvas(width: number, height: number): HTMLCanvasElement {
+        const canvas = document.createElement("canvas");
+        // Don't use CSS to set width/height, see: http://stackoverflow.com/a/12862952/69689
+        canvas.setAttribute("width", width.toString());
+        canvas.setAttribute("height", height.toString());
+        return OverlayCanvas.disableScrollbars(canvas);
+    }
+
+    constructor(width: number, height: number) {
+        this.background = OverlayCanvas.styleCanvas(OverlayCanvas.createCanvas(width, height), 1);
+        this.overlay = OverlayCanvas.styleCanvas(OverlayCanvas.createCanvas(width, height), 2);
+        this.backgroundCtx = OverlayCanvas.get2DContext(this.background);
+        this.overlayCtx = OverlayCanvas.get2DContext(this.overlay);
+    }
+
+    appendTo(element: Element): void {
+        element.appendChild(this.background);
+        element.appendChild(this.overlay);
+    }
+
+}
+
 function main(): void {
     const margin = {top: 50, right: 50, bottom: 50, left: 50};
     const borderThickness = 1;
     const [canvasW, canvasH] = calcCanvasDims(margin, borderThickness);
-    const containerElement = getCanvasContainer(margin, `${borderThickness}px solid #333`);
+    const containerElement = styleCanvasContainer(
+        getCanvasContainer(),
+        canvasW,
+        canvasH,
+        margin,
+        `${borderThickness}px solid #333`);
 
-    const canvas = createCanvas(canvasW, canvasH);
-    containerElement.appendChild(canvas);
-
-    const context = canvas.getContext("2d");
-    if (!context) {
-        throw new Error("Couldn't get 2d drawing context");
-    }
+    const canvases = new OverlayCanvas(canvasW, canvasH);
+    canvases.appendTo(containerElement);
 
     // "unit" being distance 1 on complex plane
     let pixelsPerUnit: number = getInitialScale(canvasW, canvasH);
@@ -161,23 +220,23 @@ function main(): void {
 
     const draw = () => {
         console.log(canvasW, canvasH, pixelsPerUnit, topLeftX, topLeftY);
-        drawMandlebrot(context, pixelsPerUnit, topLeftX, topLeftY);
+        drawMandlebrot(canvases.backgroundCtx, pixelsPerUnit, topLeftX, topLeftY);
     };
 
-    canvas.addEventListener("mousedown", e => {
-        const [x, y] = getMouseCoords(canvas, e);
+    canvases.overlay.addEventListener("mousedown", e => {
+        const [x, y] = getMouseCoords(canvases.overlay, e);
         mouseDownCanvasX = x;
         mouseDownCanvasY = y;
     });
 
-    canvas.addEventListener("mouseup", e => {
+    canvases.overlay.addEventListener("mouseup", e => {
         const x0 = mouseDownCanvasX / pixelsPerUnit + topLeftX;
         const y0 = mouseDownCanvasY / -pixelsPerUnit + topLeftY;
         console.log("clicked down on", x0, y0, "(complex plane coordinates)");
         topLeftX = x0;
         topLeftY = y0;
         // now use the mouseup coordinates to figure out new scale
-        const [x, y] = getMouseCoords(canvas, e);
+        const [x, y] = getMouseCoords(canvases.overlay, e);
         console.log("mouseup", mouseDownCanvasX, mouseDownCanvasY, x, y, pixelsPerUnit, (x - mouseDownCanvasX));
         pixelsPerUnit = canvasW / ((x - mouseDownCanvasX) / pixelsPerUnit);
         console.log("new ppu", pixelsPerUnit);
